651132fa137712b964be68dfe926b5236be841ed
CarlosVe97 <68933423+CarlosVe97@users.noreply.github.com>
2021-02-07 11:50:50 -0700

    Update analisis_vehiculos_pesados.R
    Agregué predicciones de series de tiempo e hice otros graficos, falta modificar los comentarios para que explique las cosas mas claras y moverle detalles a la gráficas para que se vean mas bonitas

--- pesados 2018-2020/analisis_vehiculos_pesados.R
+++ pesados 2018-2020/analisis_vehiculos_pesados.R
@@ -10,6 +10,8 @@ library('plyr')
 library("dplyr")
 library('tidyr')
 library('ggplot2')
+library('forecast')
+
 getwd()
 setwd('/home/carlos/Documents/BeduCursoR/proyecto_vehiculo/conjunto_de_datos')
 
@@ -67,7 +69,7 @@ colnames(fecha_pesados)
 datos_compactos_pesados<- fecha_pesados[,c(10,5,6,7,8,9)]
 datos_compactos_pesados
 
-
+#########################################################################
 #ANALISIS EXPLORATORIO DE DATOS(EDA)
 #Venta nacional de vehículos pesados( menudeo)
 ?ddply
@@ -161,6 +163,18 @@ grafico_energia_y_segmento <- ggplot(datos_compactos_pesados, aes(x = FUENTE_DE_
 grafico_energia_y_segmento
 
 
+#Otro grafico complejo comparango marcas, segmento y ventas
+grafico_marca_y_segmento <- ggplot(datos_compactos_pesados, aes(x = MARCA, y = MENUDEO, fill= FUENTE_DE_ENERGIA)) + 
+  geom_bar (stat="identity") +
+  ggtitle('Venta total nacional de de vehículos pesados por marca
+  (Separados por segmento)
+  2018 Ene -2020 Dic')+
+  xlab("Marca")+
+  ylab("Ventas")+
+  facet_wrap(.~ SEGMENTO)+
+  theme(axis.text.x = element_text(angle = 90, hjust = 0))
+grafico_marca_y_segmento
+
 #Filtramos los electricos e hibridos
 Electricos_e_hibridos  <- datos_compactos_pesados %>% filter((FUENTE_DE_ENERGIA== "Eléctrico")| (FUENTE_DE_ENERGIA=="Híbrido"))
 
@@ -181,3 +195,45 @@ ventas_hibridos  <- datos_compactos_pesados %>% filter(FUENTE_DE_ENERGIA=="Híbr
 head(ventas_hibridos)
 ts.ventas.hibridos <- ts(ventas_hibridos$MENUDEO,st=2018,fr=12)
 ts.plot(ts.ventas.hibridos)
+
+
+#################################################################333
+#FORECASTING
+#Despues realizaremos una prediccion de los proximos 6 meses para la venta de
+#vehiculos hibridos pesados
+autoarima_hibrido <- auto.arima(ts.ventas.hibridos)
+forecast_hibrido <- forecast(autoarima_hibrido, h = 6)
+forecast_hibrido
+plot(forecast_hibrido)
+#Al graficar los residuos vemos que hay mucha varianza apartir del segundo
+#semestre del 2019
+plot(forecast_hibrido$residuals)
+qqnorm(forecast_hibrido$residuals)
+#Al analizar el grafico de autocorrelacion vemos que solo el primero es el que 
+#esta fuera del rango
+acf(forecast_hibrido$residuals)
+pacf(forecast_hibrido$residuals)
+
+#Al analizar la exactitud de la prediccion, vemos que MAPE(error porcentual medio
+#absoluto) es infinito, esto se debe a que varios valores que son igual a 0 en
+#los meses en los que no se vendio ningun auto hibrido, ademas contemplando la alta
+#varianza y la poca cantidad de datos.
+accuracy(forecast_hibrido)
+
+#Prediccion de ventas de vehiculos pesados en los proximos 6 meses (contemplando
+#todas las fuentes de energia)
+
+autoarima_ventas_total <- auto.arima(ts.ventas.menudeo)
+autoarima_ventas_total
+forecast_ventas <- forecast(autoarima_ventas_total, h = 6)
+forecast_ventas
+plot(forecast_ventas)
+
+#vemos que tambien hay mucha varianza
+plot(forecast_ventas$residuals)
+qqnorm(forecast_ventas$residuals)
+acf(forecast_ventas$residuals)
+
+#Y ocurre el mismo problema con la exactitud
+accuracy(forecast_hibrido)
+
